eb7bb3cd46034555e32ea5b94b8d1727458e60da
ReSpix <yanovich.yakov2@gmail.com>
2025-05-18 19:56:57 +1000

    DaisyUI + обновление через форму с обратной связью

--- src/asana/client.py
+++ src/asana/client.py
@@ -2,6 +2,7 @@ import logging
 import aiohttp
 import config_manager
 
+
 class AsanaApiError(Exception):
     def __init__(self, status: int, body: dict):
         self.status = status
@@ -18,15 +19,28 @@ class AsanaApiError(Exception):
 class AsanaClient:
     base_url = "https://app.asana.com/api/1.0/"
 
-    @classmethod
-    async def check_token(cls, url, token, params={}):
+    async def _check_token(self, token, params={}):
         async with aiohttp.ClientSession() as session:
             headers = {"authorization": f"Bearer {token}"}
-            async with session.get(cls.base_url + url, headers=headers, params=params) as response:
+            async with session.get(self.base_url + 'workspaces', headers=headers, params=params) as response:
                 data = await response.json()
                 if response.status != 200:
                     raise AsanaApiError(response.status, data)
                 return data
+            
+    def get_token(self):
+        return self.token
+    
+    async def set_token(self, new_token) -> bool:
+        try:
+            await self._check_token(new_token)
+            return True
+        except AsanaApiError as e:
+            if e.status == 401:
+                logging.info("Введен неверный токен, отмена сохранения нового токена")
+                return False
+            else:
+                raise
 
     def __init__(self, token: str, main_project_gid: str):
         self.token = token
@@ -41,7 +55,7 @@ class AsanaClient:
                 if response.status != 200:
                     raise AsanaApiError(response.status, data)
                 return data
-            
+
     async def post(self, url, body, params={}):
         async with aiohttp.ClientSession() as session:
             headers = {"authorization": f"Bearer {self.token}"}
@@ -49,9 +63,10 @@ class AsanaClient:
                 data = await response.json()
                 return data
 
-
     async def workspaces(self):
         data = await self.get("workspaces")
         return data
 
-asana_client = AsanaClient(config_manager.TOKEN, config_manager.MAIN_PROJECT_GID)
+
+asana_client = AsanaClient(
+    config_manager.TOKEN, config_manager.MAIN_PROJECT_GID)
--- src/web/routes/settings.py
+++ src/web/routes/settings.py
@@ -1,25 +1,31 @@
 from fastapi import APIRouter, Request, Form
 from ..templates import settings_templates
 from config_manager import TOKEN
-import aiohttp
 from asana.client import AsanaClient, AsanaApiError
+from asana import asana_client
+import logging
 
 
 settings_router = APIRouter(prefix='/settings')
 
 
-@settings_router.get("/")
-def show(request: Request):
-    return settings_templates.TemplateResponse("index.html", {"request": request})
+@settings_router.api_route("/", methods=["GET", "POST"])
+async def submit(request: Request):
+    data = {'type': '', 'message': ''}
+    asana_token = asana_client.get_token()
 
+    if request.method == "POST":
+        form = await request.form()
+        asana_token = form.get("asana_token")
 
-@settings_router.post("/submit")
-async def submit(request: Request, asana_token=Form(...)):
-    try:
-        data = await AsanaClient.check_token("workspaces", asana_token)
-    except AsanaApiError as e:
-        if e.status == 401:
-            data = f'Неверный токен'
-        else:
-            data = f"Неизвестная ошибка:\n{e}"
+        try:
+            res = await asana_client.set_token(asana_token)
+            if res:
+                data['type'] = 'input-success'
+            else:
+                data['type'] = 'input-error'
+                data['message'] = f'Неверный токен'
+        except AsanaApiError as e:
+            data['message'] = f"Неизвестная ошибка:\n{e}"
+
     return settings_templates.TemplateResponse("index.html", {"request": request, "asana_token": asana_token, "data": data})
