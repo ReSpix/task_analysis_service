5f1b91a9953518e517588d2e212ebbea63e8720c
ReSpix <yanovich.yakov2@gmail.com>
2025-05-19 15:43:21 +1000

    Переработано для daisyui + улучшена структура шаблонов

--- src/web/templates.py
+++ src/web/templates.py
@@ -1,4 +1,21 @@
 from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
 
-ticket_templates = Jinja2Templates(directory="web/templates/tickets")
-settings_templates = Jinja2Templates(directory="web/templates/settings")
+# Указываем список путей: сначала локальная папка, затем общий корень
+ticket_env = Environment(loader=FileSystemLoader(["web/templates/tickets", "web/templates"]))
+settings_env = Environment(loader=FileSystemLoader(["web/templates/settings", "web/templates"]))
+
+# Оборачиваем вручную в Jinja2Templates
+from starlette.templating import _TemplateResponse
+
+class CustomTemplates:
+    def __init__(self, env):
+        self.env = env
+
+    def TemplateResponse(self, name, context, status_code=200):
+        template = self.env.get_template(name)
+        return _TemplateResponse(template, context, status_code=status_code)
+
+# Теперь ты можешь:
+ticket_templates = CustomTemplates(ticket_env)
+settings_templates = CustomTemplates(settings_env)
